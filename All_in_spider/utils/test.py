7547cf3845df1f4c4d429f8ecd97a0dd54fa5612
itguangzhi <itguangzhi@163.com>
2019-05-11 23:30:09 +0800

    万物皆可爬，猫眼电影数据来了

--- All_in_spider/utils/test.py
+++ All_in_spider/utils/test.py
@@ -1,59 +1,240 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from urllib import request
+# coding:utf-8
+from imp import reload
+
+__author__ = 'haoning'
+# !/usr/bin/env python
+import time
+import urllib2
+import datetime
+import requests
+import json
+import random
+import sys
+import platform
+import uuid
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 import re
-from fake_useragent import UserAgent
+import os
+import MySQLdb as mdb
+from bs4 import BeautifulSoup
 
-url = 'http://www.bjjfr.com/kezhao/'
-listpage = 6
-imgDIR = r'./kepian//'
-ypimgDIR = r'./yangpian//'
-yplistpage = 11
-ua = UserAgent()
-def getResponse(url):
-    header = {}
-    header['User-Agent'] = ua.random
-    response = request.urlopen(url,)
-    result = response.read().decode('utf-8')
-    return result
+DB_HOST = '127.0.0.1'
+DB_USER = 'root'
+DB_PASS = 'root'
+# init database
+conn = mdb.connect(DB_HOST, DB_USER, DB_PASS, 'pybbs-springboot', charset='utf8')
+conn.autocommit(False)
+curr = conn.cursor()
 
-def getDetail(response,dir):
-    reg = '<img alt="" src="/uploads/allimg/(.*?).jpg" />'
-    namereg = '<title>(.*?)_北京金夫人</title>'
-    imglist = re.findall(reg,response)
-    for imgURL in imglist:
-        url = 'http://www.bjjfr.com/uploads/allimg/%s.jpg'%str(imgURL)
-        name = str(re.findall(namereg,response)[0]).replace(' ','').replace('/','-')+'_'+imgURL.replace('/','-')
-        request.urlretrieve(url,dir + name + '.jpg')
+count = 0
+how_many = 0
+
+base_url = 'http://www.wechat-cloud.com'
+url = base_url + "/index.php?s=/home/article/ajax_get_list.html&category_id={category_id}&page={page}&size={size}"
+
+user_agents = [
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 ",
+]
+
+
+def fake_header():
+    agent = random.choice(user_agents)
+    cookie = 'PHPSESSID=p5mokvec7ct1gqe9efcnth9d44; Hm_lvt_c364957e96174b029f292041f7d822b7=1487492811,1487556626; Hm_lpvt_c364957e96174b029f292041f7d822b7=1487564069'
+    req_header = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        # 'Accept-Encoding':'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'www.wechat-cloud.com',
+        # 'Cookie':cookie,
+        'Referer': 'http://www.wechat-cloud.com/index.php?s=/home/index/index.html',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': agent,
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    return req_header
+
+
+def gethtml(url):
+    try:
+        header = fake_header()
+        req = urllib2.Request(url, headers=header)
+        response = urllib2.urlopen(req, None, 15)
+        html = response.read()
+        return html
+    except Exception as e:
+        print
+        "e", e
+    return None
+
+
+def get_img_data(url):
+    try:
+        # 添加头信息，模仿浏览器抓取网页，对付返回403禁止访问的问题
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req, None, 15)
+        dataimg = response.read()
+        return dataimg
+    except Exception as e:
+        print
+        "image data", e
+    return None
+
+
+def makeDateFolder(par, classify):
+    try:
+        if os.path.isdir(par):
+            newFolderName = par + '//' + str(classify) + "//" + GetDateString()
+            if not os.path.isdir(newFolderName):
+                os.makedirs(newFolderName)
+            return newFolderName
+        else:
+            return par
+    except Exception as e:
+        print( "kk", e)
+    return par
+
+
+def map_folder(what):
+    return what
+
+
+def GetDateString():
+    when = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    foldername = str(when)
+    return foldername
+
+
+def get_extension(name):
+    where = name.rfind('.')
+    if where != -1:
+        return name[where:len(name)]
+    return "#"
+
+
+def download_img(url, what):
+    try:
+        # print url
+        extention = get_extension(url)
+        dataimg = get_img_data(url)
+        name = str(uuid.uuid1()).replace('-', '') + "-www.weixinapphome.com"
+        # print "name",name
+        classfiy_folder = map_folder(what)
+        top = "E://wxapp_store"
+        filename = makeDateFolder(top, classfiy_folder) + "//" + name + extention
+        try:
+            if not os.path.exists(filename):
+                file_object = open(filename, 'w+b')
+                file_object.write(dataimg)
+                file_object.close()
+                return classfiy_folder + "/" + GetDateString() + "/" + name + extention
+            else:
+                print("file exist")
+                return None
+        except IOError as e1:
+            print("e1=", e1)
+            # pass
+        return None  # 如果没有下载下来就利用原来网站的链接
+    except Exception as e:
+        print("problem", e)
+        pass
+    return None
+
+
+def work():
+    page = 0
+    global how_many
+    while 1:
+        try:
+            page = page + 1
+            begin_url = url.format(category_id=0, page=page, size=12).encode('utf-8')
+            html = gethtml(begin_url)
+            if html is not None:
+                # print html
+                json_results = json.loads(html)
+                is_end = json_results['isEnd']
+                if str(is_end) == "True":
+                    break
+                results = json_results['list']
+                for result in results:
+                    href = result['href']
+                    detail_url = base_url + href
+                    # print detail_url
+                    detail_html = gethtml(detail_url)
+                    if detail_html is not None:
+                        soup = BeautifulSoup(detail_html)
+                        icon_url = base_url + soup.find('div', {'class': 'icon fl'}).find('img').get('src')
+                        name = soup.find('div', {'class': 'cont fl'}).find('h2').text
+                        classify = soup.find('div', {'class': 'tab'}).find('span').text
+                        classify = str(classify).replace("分类: ", "")
+                        # print classify
+                        barcode_path = base_url + soup.find('div', {'id': 'install-code'}).find('img').get('src')
+                        view_num = soup.find('span', {'class': 'views'}).text
+                        # view_num=filter(str.isalnum,str(view_num))
+                        pic_path = base_url + soup.find('div', {'class': 'img-box'}).find('img').get('src')
+                        temp = time.time()
+                        x = time.localtime(float(temp))
+                        acq_time = time.strftime("%Y-%m-%d %H:%M:%S", x)  # get time now
+                        curr.execute('select id from pybbs_wxapp_store where `from`=%s', (detail_url))
+                        y = curr.fetchone()
+                        if not y:
+                            y1 = download_img(icon_url, "icon")
+                            y2 = download_img(barcode_path, "barcode")
+                            y3 = download_img(pic_path, "pic")
+                            if (y1 is not None) and (y2 is not None) and (y3 is not None):
+                                name = name
+                                author = None
+                                classify = classify
+                                describe = None
+                                view_num = view_num
+                                # print view_num
+                                logo = y1
+                                _from = detail_url
+                                barcode = y2
+                                acq_time = acq_time
+                                hot_weight = -9999
+                                pic_uuid = str(uuid.uuid1()).replace('-', '')
+                                pic_path = y3
+                                # print name,author,classify,describe,view_num,logo,_from,barcode,acq_time,hot_weight,pic_uuid
+                                curr.execute(
+                                    'INSERT INTO pybbs_wxapp_store(name,author,classify,`describe`,view_num,logo,`from`,barcode,acq_time,hot_weight,pic_path)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+                                    (name, author, classify, describe, view_num, logo, _from, barcode, acq_time,
+                                     hot_weight, pic_path))
+                                curr.execute('select id from pybbs_wxapp_classify where `classify_name`=%s', (classify))
+                                yx = curr.fetchone()
+                                if not yx:
+                                    describe = None
+                                    temp = time.time()
+                                    x = time.localtime(float(temp))
+                                    record_time = time.strftime("%Y-%m-%d %H:%M:%S", x)  # get time now
+                                    curr.execute(
+                                        'INSERT INTO pybbs_wxapp_classify(classify_name,`describe`,record_time)VALUES(%s,%s,%s)',
+                                        (classify, describe, record_time))
+                                how_many += 1
+                                print
+                                "new comer:", pic_uuid, ">>", how_many
+                                if how_many % 10 == 0:
+                                    conn.commit()
+                conn.commit()
+        except Exception as e:
+            print
+            "while error", e
+
 
 if __name__ == '__main__':
-    # 客片
-    for i in range(1,listpage+1):
-        url = 'http://www.bjjfr.com/kezhao/'
-        url = url + 'list'+str(i)+ '.html'
-        result = getResponse(url)
-        reg = '<a href="/kezhao/(.*?).html" target="_blank"><img src="/uploads/allimg/.*?" alt=".*?客片"></a>'
-        kezhao = re.findall(reg,str(result))
-        kezhao.append('1890')
-        kezhao.append('1891')
-        kezhao.append('1892')
-        for kezhaoimg in kezhao:
-            detailURL = 'http://www.bjjfr.com/kezhao/%s.html'%str(kezhaoimg)
-            detail = getResponse(detailURL)
-            res = getDetail(detail,imgDIR)
-            print(detailURL)
-
-        print(url, 'OK')
-
-    # 样片
-    for a in range(1,yplistpage+1):
-        ypurl = 'http://www.bjjfr.com/zp/list%d.html'%a
-        ypresult = getResponse(ypurl)
-        ypreg = '<a href="/zuopin/(.*?).html" target="_blank"><img src=".*?" alt=""></a>'
-        yplist = re.findall(ypreg,str(ypresult))
-        for n in yplist:
-            ypdetailurl = 'http://www.bjjfr.com/zuopin/%s.html'%str(n)
-            ypdetail = getResponse(ypdetailurl)
-            res = getDetail(ypdetail, ypimgDIR)
-            print(ypdetailurl)
-        print(ypurl, 'OK')
+    i = 3
+    while i > 0:
+        work()
+        i = i - 1
